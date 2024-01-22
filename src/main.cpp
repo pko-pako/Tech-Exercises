e4b7b08c9d2adcd0ddec0b152b6e96bd260e88bc
Pavel Kostadinov <pko@milestone.dk>
2024-01-22 14:47:55 +0200

    zemi tva asio Vanka

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <boost/asio.hpp>
+
 #include "actionsStatic.h"
 #include "actionsDynamic.h"
 
@@ -8,5 +10,8 @@ int main(int argc, char *argv[])
 
    std::cout << "tosho is " << t.b << " 3+4=" << sum(3,4) << " 3 ravno li e na 3 = " << equals(3,3) <<std::endl;
    std::cout << "7 po 4 e" << multiply(7,4) << std::endl;
+
+   boost::asio::io_context io_context(1);
+
    return 0;
 }
