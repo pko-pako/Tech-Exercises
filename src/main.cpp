d4061df59152660f95013f45046e71718bfe30bc
Pavel Kostadinov <pko@milestone.dk>
2024-02-14 11:27:55 +0200

    dsadsad

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,8 @@
 
 int main(int argc, char *argv[])
 {
-   
-	
-	Tosho t(42);
-
+   Tosho t(42);
+   // dalsj aslkjd
    std::cout << "tosho is " << t.b << " 3+4=" << sum(3,4) << " 3 ravno li e na 3 = " << equals(3,3) <<std::endl;
    std::cout << "7 po 4 e" << multiply(7,4) << std::endl;
 
