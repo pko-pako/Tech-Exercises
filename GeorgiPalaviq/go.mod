29f72798c2379ca63e669fa87301cf2734e52b6a
Pavel Kostadinov <pko@milestone.dk>
2024-06-14 13:44:48 +0300

    update

---
+++ GeorgiPalaviq/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.22.4
+
+require github.com/beevik/etree v1.1.0 // indirect
---
+++ GeorgiPalaviq/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/beevik/etree"
+)
+
+func main() {
+	etree.Document()
+}
