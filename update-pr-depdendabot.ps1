79cb72441175d8ae2cb01ec7eeb2dc7e99fc77a5
Pavel Kostadinov <pko@milestone.dk>
2024-06-19 12:52:20 +0300

    add action

--- update-pr-depdendabot.ps1
+++ update-pr-depdendabot.ps1
@@ -3,51 +3,44 @@ param (
     [int]$prNumber
 )
 
-# Change to the repository directory (assumed to be the current location)
-Set-Location -Path './'
+$repository = "pko-pako/Tech-Exercises"
 
 # Check if user is logged into github
-if (-not gh auth status --hostname "github.com"){
-	Write-Host "You're not authenticated, please login following the steps.`nIf you choose not to use credentials, mobile app authentication will be used."
-	gh auth login --git-protocol HTTPS --hostname "github.com" --web
+if (-not (gh auth status --hostname "github.com")){
+	Write-Host "You're not authenticated, please login following the steps.`nIf you choose not to use credentials, faster authentication will be used."
+	gh auth login --git-protocol HTTPS --hostname "github.com" --scopes 'workflow' --web
 }
 
 # Get the original pull request details
-$sourcePr = gh pr view $prNumber --json headRefName,title,body | ConvertFrom-Json
-$sourceBranch = $sourcePr.headRefName
-$sourcePrTitle = $sourcePr.title
-$sourcePrBody = $sourcePr.body
+$sourcePr = gh pr view $prNumber --repo $repository --json headRefName,title,body,headRefOid | ConvertFrom-Json
+$branch = $sourcePr.headRefName
+$title = $sourcePr.title
+$body = $sourcePr.body
+$commitSha = $sourcePr.headRefOid
 
 # Check if the name of the branch starts with "dependabot/"
 $requiredPrefix = "dependabot/"
-if (-not ($sourceBranch.StartsWith($requiredPrefix))) {
+if (-not ($branch.StartsWith($requiredPrefix))) {
     Write-Host "PR branch does not start with '$requiredPrefix'. Exiting script."
     exit
 }
 
-$newBranch = "scripted/" + $sourceBranch
+# Close the original pull request
+gh pr close --repo $repository $prNumber
 
-# Pull the latest changes from the source branch
-git checkout $sourceBranch
-git pull origin $sourceBranch
+# Wait for dependabot to delete the branch
+Start-Sleep -Seconds 10.0
 
-# Create and switch to the new branch
-git checkout -b $newBranch
+# Restore branch
+gh api "repos/$repository/git/refs" -f ref="refs/heads/$branch" -f sha="$commitSha"
+$exitCode = $LASTEXITCODE
 
-# Merge changes from the source branch to the new branch
-git merge $sourceBranch
-
-# Push the new branch to the origin
-git push -u origin $newBranch
+# Check if the restore was successful
+if ($exitCode -ne 0) {
+	Write-Output "Unsuccessful restore of branch.`nPlease authorize again"
+	gh auth login --git-protocol HTTPS --hostname "github.com" --scopes 'workflow' --web
+	exit
+}
 
 # Create a new pull request for the new branch with the same title and body as the original
-$newPrUrl = gh pr create --title "$sourcePrTitle" --body "$sourcePrBody" --base main --head $newBranch --repo "milestone-systems-as/DeviceIntegrationServices"
-
-# Close the original pull request
-gh pr close $prNumber
-
-# Output the URL of the new pull request
-Write-Output "New pull request created: $newPrUrl"
-
-# Open the url of the pull request
-Start-Process $newPrUrl
+gh pr create --title "$title" --base main --repo "$repository" --head "$branch" -w
